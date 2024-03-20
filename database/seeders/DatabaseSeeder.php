662a7def1060a3625c24e4e72b334c06ff9aac1c
MAH Dev <95893857+arihidayatm@users.noreply.github.com>
2024-03-20 12:30:24 +0700

    Add files via upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'mah@fic15.com',
+            'role' => 'admin',
+            'password' => Hash::make('123456'),
+            'phone' => '085274149119',
+        ]);
+    }
+}
