d237651f54f960b4b480e2b7c6d010de78c1208a
arihidayatm <muhamadarihidayat@gmail.com>
2024-03-22 10:52:47 +0700

    Creating Migration table - profile_clinic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,19 +13,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        //otomatis tambah dummy data
+        //\App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
+        
+        //manual tambah data user
         \App\Models\User::factory()->create([
             'name' => 'Admin User',
             'email' => 'mah@fic15.com',
             'role' => 'admin',
             'password' => Hash::make('123456'),
             'phone' => '085274149119',
+        ]);
+
+        //manual tambah data profile clinic
+        \App\Models\ProfileClinic::factory()->create([
+            'name' => 'MAH CLINIC',
+            'address' => 'Jalan Kesehatan adalah keinginan kita semua',
+            'phone' => '085274149119',
+            'email' => 'muhamadarihidayat@gmail.com',
+            'doctor_name' => 'Muhamad Ari Hidayat',
+            'clinic_code' => ('280789'),
         ]);
     }
 }
