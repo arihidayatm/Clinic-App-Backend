27908c815fe2dcc413e5a97afb8171871f2de189
arihidayatm <muhamadarihidayat@gmail.com>
2024-03-29 04:03:05 +0700

    Make Route DoctorScheduleController::class in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\DoctorScheduleController;
 
 Route::get('/', function () {
     return view('pages.auth.login');
@@ -21,4 +22,6 @@ Route::middleware(['auth'])->group(function(){
 
     //doctor
     Route::resource('doctors', DoctorController::class);
+    //doctor-schedule
+    Route::resource('doctor-schedules', DoctorScheduleController::class);
 });
